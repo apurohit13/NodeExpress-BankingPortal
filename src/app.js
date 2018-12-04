0d14e29cefc551bee55ccd6465af126bc8d5b872
apurohit13 <purohit.amit@gmail.com>
2018-12-04 09:54:06 +0000

    Completed the module

--- src/app.js
+++ src/app.js
@@ -10,8 +10,10 @@ const {
     writeJSON
 } = require("./data");
 
+const accountRoutes = require('./routes/accounts.js');
+const servicesRoutes = require('./routes/services.js');
 
-var port = process.env.port || 3000;
+var port = process.env.port || 8080;
 
 app.set('views', path.join(__dirname, '../src/views'));
 app.set('view engine', 'ejs');
@@ -20,13 +22,10 @@ app.use('public', express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
 app.use('/js', express.static(path.join(__dirname, 'public', 'js')));
 
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
+
 app.use(express.urlencoded({ extended: true }));
-
-// const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'UTF8');
-// const accounts = JSON.parse(accountData);
-
-// const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'UTF8');
-// const users = JSON.parse(userData);
 
 app.get('/', function(req, res) {
     res.render('index', {
@@ -35,48 +34,9 @@ app.get('/', function(req, res) {
     });
 })
 
-app.get('/savings', function(req, res) {
-    res.render('account', { account: 'accounts.savings' });
-})
-
-app.get('/checking', function(req, res) {
-    res.render('account', { account: 'accounts.checking' });
-})
-
-app.get('/credit', function(req, res) {
-    res.render('account', { account: 'accounts.credit' });
-})
-
-app.get('/savings', function(req, res) {
-    res.render('account', { account: 'accounts.savings' });
-})
 
 app.get('/profile', function(req, res) {
     res.render('profile', { user: users[0] });
-})
-
-app.get('/transfer', function(req, res) {
-    res.render('transfer');
-})
-
-app.post('/transfer', function(req, res) {
-    var from = req.body.from;
-    var to = req.body.to;
-    accounts[from].balance = parseInt(accounts[from].balance) - parseInt(req.body.amount);;
-    accounts[to].balance = parseInt(accounts[to].balance) + parseInt(req.body.amount);
-    writeJSON();
-    res.render('transfer', { message: "Transfer Completed" })
-})
-
-app.get('/payment', function(req, res) {
-    res.render('payment', { account: accounts.credit });
-})
-
-app.post('/payment', function(req, res) {
-    accounts.credit.balance = parseInt(accounts.credit.balance) - parseInt(req.body.amount);
-    accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
-    writeJSON();
-    res.render('payment', { message: "Payment Successful", account: accounts.credit })
 })
 
 app.listen(port, function() {
